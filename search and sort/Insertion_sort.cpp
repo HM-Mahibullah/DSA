8cd005ef5651c8bb8056cdcef21784550e4c33a6
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-05-11 19:55:46 +0600

    Add files via upload

---
+++ search and sort/Insertion_sort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Insertion_sort(int a[], int size)
+{
+  int item;
+  for(int i=1;i<size;i++)
+  {
+    item=a[i];
+    int j=i-1;
+    while(j>=0 && a[j]>item)
+    {
+     a[j+1]=a[j];
+     j=j-1;
+    }
+    a[j+1]=item;
+    
+  }
+}
+int main()
+{
+    int a[5];
+    int size=sizeof(a)/sizeof(a[0]);
+    cout<<"Enter Five Elements";
+    for(int  i=0;i<size;i++)
+    {
+        cin>>a[i];
+    }
+    
+  
+    int index=Insertion_sort(a,size);
+   
+for(int b:a)
+{
+    cout<<b;
+}
+}
