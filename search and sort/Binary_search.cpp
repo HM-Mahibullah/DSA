b963f30625c93cfe803c7b1be9534cd485c4aebc
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-05-11 17:09:40 +0600

    Add files via upload

---
+++ search and sort/Binary_search.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binary_search(int a[],int check, int size)
+{
+   int left=1,right=size,mid;
+   while (left<=right)
+   {
+    mid=((left+right)/2);
+    if(a[mid]==check)
+    {
+        return mid;
+    }
+   if (a[mid] < check)
+   {
+            left = mid + 1;
+   }
+        else
+        {
+            right = mid - 1;
+        }
+   }
+   return -1;
+
+}
+int main()
+{
+    int a[]={1,2,3,4,5,6,7,8,9},number=9;
+    int size=sizeof(a)/sizeof(a[0]);
+    cout<<size<<endl;
+  
+    int index=binary_search(a,number,size);
+    if(index!=-1)
+    {
+        cout<<"The correct index is="<<index<<endl;
+    }
+    else
+    {
+        cout<<"Can't find";
+    }
+}
