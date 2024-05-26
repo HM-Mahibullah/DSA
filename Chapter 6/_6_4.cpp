a1794ad9ccad974f87aec26e009c0b2ab3e16520
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-05-26 23:25:59 +0600

    Update _6_4.cpp

--- Chapter 6/_6_4.cpp
+++ Chapter 6/_6_4.cpp
@@ -2,13 +2,36 @@
 using namespace std;
 
 
-int main() //push elements in stack
+int main() //push elements integer type in stack
 {
   
     stack<int> a;
     a.push(10);
     a.push(20);
     a.push(30);
+    while(!a.empty())
+    {
+    cout<<"Top element:"<<a.top()<<endl;
+    a.pop();
+    }
+      
+    
+    return 0;
+}
+
+//push element string type in stack
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+  
+    stack<string> a;
+    a.push("Mahibullah");
+    a.push("sharafat");
+    a.push("Kaiyum");
     while(!a.empty())
     {
     cout<<"Top element:"<<a.top()<<endl;
