0c427b580578b7db1e5d3f838a56cd016e46a45a
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-06-10 11:11:29 +0600

    Update _6_3.cpp

--- Chapter 6/_6_3.cpp
+++ Chapter 6/_6_3.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int evaluatePostfix( string& postfix) {
     stack<int> s;
-    istringstream partString(postfix);//Allow every string read.
+    istringstream partString(postfix);//Allow every string read whthout space.
     string token;
 
     while (partString >> token) {
-        if (isdigit(token[0]) || (token.size() > 1 && isdigit(token[1]))) //is digit check true or false
+        if (isdigit(token[0]) || (token.size() > 1 && isdigit(token[1]))) //isdigit check true or false
         {
-            // Token is an operand (handling multi-digit numbers)
             s.push(stoi(token));// stoi is standerd library which convert character to digit.
         } else {
             // Token is an operator
@@ -45,7 +44,7 @@ int evaluatePostfix( string& postfix) {
 int main() {
     // Example postfix expression
    // string postFix = "5 10 + 6 12 - *";
-    string postFix = "5 6 2 + * 12 4 / -";//from book
+    string postFix = "5 6 2 + * 12 4 / -";//from book page 170
 
     int result = evaluatePostfix(postFix);
     cout << result << endl;
