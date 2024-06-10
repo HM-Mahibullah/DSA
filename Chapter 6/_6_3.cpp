9beb889b2f153a67571e842efa911363349ed0e6
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-06-10 11:09:32 +0600

    Add files via upload

---
+++ Chapter 6/_6_3.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int evaluatePostfix( string& postfix) {
+    stack<int> s;
+    istringstream partString(postfix);//Allow every string read.
+    string token;
+
+    while (partString >> token) {
+        if (isdigit(token[0]) || (token.size() > 1 && isdigit(token[1]))) //is digit check true or false
+        {
+            // Token is an operand (handling multi-digit numbers)
+            s.push(stoi(token));// stoi is standerd library which convert character to digit.
+        } else {
+            // Token is an operator
+            int A = s.top();
+            s.pop();
+            int B = s.top();
+            s.pop();
+
+            switch (token[0])
+            {
+                case '+':
+                    s.push(B + A);
+                    break;
+                case '-':
+                    s.push(B - A);
+                    break;
+                case '*':
+                    s.push(B * A);
+                    break;
+                case '/':
+                    s.push(B / A);
+                    break;
+            }
+        }
+    }
+
+    int value = s.top();
+    s.pop();
+
+    return value;
+}
+
+int main() {
+    // Example postfix expression
+   // string postFix = "5 10 + 6 12 - *";
+    string postFix = "5 6 2 + * 12 4 / -";//from book
+
+    int result = evaluatePostfix(postFix);
+    cout << result << endl;
+
+    return 0;
+}
