4814f7d9f0715bd3c8253a0ddb44b3ebeab32b7f
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-06-21 23:14:09 +0600

    Update _2.1.cpp

--- chapter-2/_2.1.cpp
+++ chapter-2/_2.1.cpp
@@ -1,13 +1,16 @@
+//Book page 21
+
 #include<bits/stdc++.h>
 using namespace std;
 void LargestValueOfArray(int Data[],int N)
 {
-    int K=0,Loc=0,Max=Data[0];
+    int K=0,Loc=0,Max=Data[0];   int K = 3; // if k=3 then Start the loop from the 4th element (0-based index)
     Increment_Counter:
     K+=1;
     if(K==N)
     {
-        cout<<"Loc"<<Loc<<"Max,"<<Max<<"\n";
+        cout<<"Loc="<<Loc<<endl<<"Max="<<Max<<"\n";
+        return;  // Properly break out of the function
     }
     if(Max<Data[K])
     {
@@ -19,8 +22,8 @@ void LargestValueOfArray(int Data[],int N)
 }
 int main()
 {
-int Data[]={2,3,4,6};
-int N=sizeof(Data);
+int Data[]={2,3,4,9,1,0,10};
+int N=sizeof(Data)/sizeof(Data[0]);
  LargestValueOfArray(Data,N);
 
 }
