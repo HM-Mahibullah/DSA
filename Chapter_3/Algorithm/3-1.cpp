b30d104f1807ae79f856c92e21734e9fc6c46ad0
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-06-27 22:23:44 +0600

    Add files via upload

---
+++ Chapter_3/Algorithm/3-1.cpp
@@ -0,0 +1,34 @@
+/*
+51 pages 
+3.1 Algorithm
+
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string T, P;
+    cout << "Enter your Text\n";
+    //getline(cin,T);// Reads entire line including spaces
+    cout << "Enter your keyword\n";
+    cin >> P;
+    size_t K = T.find(P);//size_t is an unsigned integer type, which means it cannot represent negative numbers.
+    while(K != -1)
+    {
+        T.erase(K, P.length());
+        K = T.find(P);
+    }
+    cout << T << endl;
+    return 0;
+}
+//main end
+
+/*
+output
+Enter your Text
+ZAAABBBY
+Enter your keyword
+AB
+ZY
+*/
