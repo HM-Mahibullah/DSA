fdde7ea4b9d7fd83ad3c84b6eafa595ac91e8b73
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-08-11 10:53:21 +0600

    Update 5-3.cpp

--- Chapter-5/Algorithm/5-3.cpp
+++ Chapter-5/Algorithm/5-3.cpp
@@ -9,33 +9,75 @@ LIST is a sorted list in memory. This algorithm finds the location LOC of the no
  5. Exit. 
  */
 
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 struct Node
 {
-    int data;
-    Node *next;
+int data;
+Node* next;
 };
-Node *Head=NULL;
+int count=0;
+Node* Head=nullptr;
+Node* Avail=nullptr;
 void PRINT()
 {
-    Node *Temp=Head;
-    while (Temp!=NULL)
-    {
-     cout << "Node data: " << Temp->data
-        << ", Node address: " << Temp
-        << ", Next node address: " << Temp->next << endl;
-     Temp=Temp->next;
-    }
-    
+ Node* Temp=Head;
+ while (Temp!=nullptr)
+ {
+    cout<<"Node element="<<Temp->data
+    <<" Next node address="<<Temp->next<<endl;
+    Temp=Temp->next;
+ }
 }
-
+Node* GetNode()
+{
+    if(Avail!=nullptr)
+    {
+        Node* node=Avail;
+        Avail=Avail->next;
+        return node;
+    }
+    else
+    {
+        return new Node();
+    }
+}
+/* void FirstElement(int element)
+{
+    Node* insertnode=GetNode();
+    insertnode->data=element;
+    insertnode->next=Head;
+    Head=insertnode;
+} */
+void InsertSorted(Node* &Head,int element)
+{
+  Node* newnode=GetNode();
+  newnode->data=element;
+  newnode->next=nullptr;
+  if(Head==nullptr||element<= Head->data)
+  {
+    newnode->next=Head;
+    Head=newnode;
+  }
+  else
+  {
+    Node* current=Head;
+    while (current!=nullptr&& element> current->next->data)
+    {
+     current=current->next;
+    }
+    newnode->next=current->next;
+    current->next=newnode;
+    
+  }
+}
 
 Node *SEARCH(int element)
 {
 Node *Temp=Head;
 while (Temp!=NULL)//NULL is used to indicate the end of the list, meaning there are no more nodes to process.
 {
+    count++;
 
    if(Temp->data<element)
     {
@@ -47,70 +89,46 @@ while (Temp!=NULL)//NULL is used to indicate the end of the list, meaning there 
     }
     else{
         return NULL;
-    } 
-   
-
-
-/*  
-                     //For 5.2 Algotithm//
-   if(Temp->data==element)
-    {
-        return Temp;//it returns Temp node that contains data and pointer
     }
-   Temp= Temp->next; 
-   */
 }
-//return NULL;
-
 }
 int main()
 {
-int x,n;
-cout<<"Enter Creating Numbers=";
-cin>>n;
-cout<<"Enter  Node 1 element=";
-cin>>x;
-Node *node1=new Node();
-node1->data=x;
-node1->next=NULL;
-Head=node1;
-Node *currentNode=node1;
-for (int  i =2; i <=n; i++)
-{
-    cout<<"Enter Node "<<i<<"element=";
-    cin>>x;
-    Node *newnode=new Node();
-    newnode->data=x;
-    newnode->next=NULL;
-    currentNode->next=newnode;
-    currentNode=newnode;
-
-}
-PRINT();
-
-//Find the location an elemnet in link list which is unsort.
-int element;
-cout<<"Enter the element for search=";
-cin>>element;
-Node* result=SEARCH(element);//It receives pointer type Node which is Temp(data,address)
-if(result!=NULL)
-{
-    cout<<"Element "<<element<<" is  found and address is="<<result<<endl;
-    cout<<"This node element is="<<result->data;
-}
-else
-{
-    cout<<"Element "<<element<<"is not find in the  list";
-}
-
+    int n,x,element;
+    cout<<"Enter total node numbers=";
+    cin>>n;
+    for (int  i=1;i<=n; i++)
+    {
+     cout << "Enter element for Node " << i << ": ";
+    cin >> x;
+    InsertSorted(Head,x);
+    }
+  PRINT();
+  
+/*   cout<<"At the beggining insert element in the node=";
+  cin>>element;
+  FirstElement(element);
+  PRINT(); */
+   // Find the location of an element in the linked list which is sorted.
+    cout << "Enter the element to search: ";
+    cin >> element;
+    Node* result = SEARCH(element); // It receives pointer type Node which is Temp(data, address)
+    if (result != nullptr) {
+        cout << "Element " << element << " is found and address is = " << result << endl;
+        cout << "This node element is = " << result->data <<" Node number is="<<count<< endl;
+    } else {
+        cout << "Element " << element << " is not found in the list." << endl;
+    }
 
 
 }
-
-/* Enter Creating Numbers=3
-Enter  Node 1 element=2
-Enter Node 2element=2
-Enter Node 3element=5
-Node data: 2, Node address: 0x10c16c0, Next node address: 0x10c16d0
-Node data: 2, Node address: 0x10c16d0, Next node address: 0x10c6f78
-Node data: 5, Node address: 0x10c6f78, Next node address: 0 */
+/* Enter total node numbers=3
+Enter element for Node 1: 6
+Enter element for Node 2: 5
+Enter element for Node 3: 4
+Node element=4 Next node address=0xfd6f20
+Node element=5 Next node address=0xfd6f10
+Node element=6 Next node address=0
+Enter the element to search: 6
+Element 6 is found and address is = 0xfd6f10
+This node element is = 6Node number is=3 */
