4da6e5dedd33b6fa15666b681ab3519a6c3e95ae
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-10-13 13:06:10 +0600

    Update 3-3.cpp

--- Chapter-3/Algorithm/3-3.cpp
+++ Chapter-3/Algorithm/3-3.cpp
@@ -1,7 +1,7 @@
 /*
 Page  54
-Algorithm 3.3: (Pattern Matching) P and T are strings with lengths R and S, 
-respectively, and are stored as arrays with one character per element. This 
+Algorithm 3.3: (Pattern Matching) P and T are strings with lengths R and S,
+respectively, and are stored as arrays with one character per element. This
 algorithm finds the INDEX of P in T.
 
 1. [Initialize.] Set K := 1 and MAX : = S − R + 1.
@@ -15,57 +15,56 @@ algorithm finds the INDEX of P in T.
 6. [Failure.] Set INDEX = 0.
 7. Exit.
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// int j;     //using global variable
 int main()
 {
-    string string,subString;
-    cout<<"Enter main string=";
-    cin>>string;
-    cout<<"Enter the checking string=";
-    cin>>subString;
-    int stringLength=string.length();
-    int subStringLength=subString.length();
-    int max=stringLength-subStringLength+1;
-    int loopCounter=0, index=-1;
-    for(int i=0;i<max;i++)
+    string string, subString;
+    cout << "Enter main string=";
+    cin >> string;
+    cout << "Enter the checking string=";
+    cin >> subString;
+    int stringLength = string.length();
+    int subStringLength = subString.length();
+    int max = stringLength - subStringLength + 1;
+    int loopCounter = 0, index = -1;
+    for (int i = 0; i < max; i++)
     {
-           int j;
-        for(j=0;j<subStringLength;++j)
+        int j; // like as global variable
+        for (j = 0; j < subStringLength; ++j)
         {
             loopCounter++;
-       
-          if(subString[j] != string[i+j])  
-          {
-           break;
-          }
-       
+
+            if (subString[j] != string[i + j])
+            {
+                break;
+            }
         }
-         if(j==subStringLength)
+        if (j == subStringLength)
         {
-         index=i;
-         break;
+            index = i;
+            break;
         }
     }
-    if(index!=-1)
+    if (index != -1)
     {
-      cout<<"Find the index which for matching is="<<index+1<<endl;
-       cout<<"Total checking iteration loop="<<loopCounter;
+        cout << "Find the index which for matching is=" << index + 1 << endl;
+        cout << "Total checking iteration loop=" << loopCounter;
     }
     else
     {
-     cout<<"   **Can't found index for pattern matching algorithm**"<<endl;
-     cout<<"Total checking iteration loop="<<loopCounter;
+        cout << "   **Can't found index for pattern matching algorithm**" << endl;
+        cout << "Total checking iteration loop=" << loopCounter;
     }
-
 }
 /*
 input:ababababab
     abc
 output:**Can't found index for pattern matching algorithm**"
         "Total checking iteration loop=16
-        
-        
+
+
 input:aabbaabbaabb
       aaa
 output:**Can't found index for pattern matching algorithm**"
@@ -73,9 +72,9 @@ output:**Can't found index for pattern matching algorithm**"
 
 input:abaabbaaabbbaaaabbbb
        aaa
-       
+
 output:Find the index which for matching is=7
-        "Total checking iteration loop=13      
+        "Total checking iteration loop=13
         */
 
 /*
@@ -87,7 +86,7 @@ int main()
 {
     string T, P;
     size_t K, MAX, S, R,loopcounter=0, INDEX = 0;
-    
+
 
     cout << "Enter your Text\n";
     getline(cin,T);
@@ -95,9 +94,9 @@ int main()
     cin >> P;
     S = T.length(); //length of Main string
     R = P.length(); // Length of sub string
-    K = 0; 
+    K = 0;
     MAX = S - R + 1;
-    while(K <MAX)// while(K <=MAX) means loop execute equal to  max 
+    while(K <MAX)// while(K <=MAX) means loop execute equal to  max
     {
         for(int L = 0; L < R; L++)
         {
@@ -109,7 +108,7 @@ int main()
         break;
     Lable:
         K++;
-         
+
     }
     cout << "Index = " << INDEX << endl;
     cout << "Loop Count = " << loopcounter<< endl;
