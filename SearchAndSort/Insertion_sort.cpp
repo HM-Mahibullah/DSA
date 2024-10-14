134cd48b12f6f494b3d0022c9973fd6b147aed29
HM-Mahibullah <165460378+HM-Mahibullah@users.noreply.github.com>
2024-10-14 18:45:21 +0600

    Update Insertion_sort.cpp

--- SearchAndSort/Insertion_sort.cpp
+++ SearchAndSort/Insertion_sort.cpp
@@ -1,39 +1,37 @@
-//https://www.youtube.com/watch?v=iQNZAOw67zM&t=690s
-#include<bits/stdc++.h>
+// https://www.youtube.com/watch?v=iQNZAOw67zM&t=690s
+#include <bits/stdc++.h>
 using namespace std;
 int Insertion_sort(int a[], int size)
 {
-  int item;
-  for(int i=1;i<size;i++)
-  {
-    item=a[i];
-    int j=i-1;
-    while(j>=0 && a[j]>item)
-    {
-     a[j+1]=a[j];
-     j=j-1;// or j--;
-    }
-    a[j+1]=item;
-    
-  }
+	int item;
+	for (int i = 1; i < size; i++)
+	{
+		item = a[i];
+		int j = i - 1;
+		while (j >= 0 && a[j] > item)
+		{
+			a[j + 1] = a[j];
+			j = j - 1; // or j--;
+		}
+		a[j + 1] = item;
+	}
 }
 int main()
 {
-    int a[5];//Array is fixed length data types
-    int size=sizeof(a)/sizeof(a[0]);
-    cout<<"Enter Five Elements";
-    for(int  i=0;i<size;i++)
-    {
-        cin>>a[i];
-    }
-    
-  
-    int index=Insertion_sort(a,size);
- cout<<"After sorting array=";  
-for(int b:a)
-{
-    cout<<b<<" ";
-}
+	int a[5]; // Array is fixed length data types
+	int size = sizeof(a) / sizeof(a[0]);
+	cout << "Enter Five Elements";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> a[i];
+	}
+
+	int index = Insertion_sort(a, size);
+	cout << "After sorting array=";
+	for (int b : a)
+	{
+		cout << b << " ";
+	}
 }
 
 /*
